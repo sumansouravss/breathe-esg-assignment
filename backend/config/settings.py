97db977f3ed5579fe2ed0f8b1099a40b3cb3ba17
suman <sumansouravrs002@gmail.com>
2026-05-30 01:06:52 +0530

    fix 1

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -121,5 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files
+
+STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 CORS_ALLOW_ALL_ORIGINS = True
